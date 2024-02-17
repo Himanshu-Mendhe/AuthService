0d18bee6d095295fb503f61d0156ab6a897f56b0
Himanshu-Mendhe <himanshu.afcwp23@sinhgad.edu>
2024-02-17 16:26:15 +0530

    minor error fix

--- src/controllers/user-controllers.js
+++ src/controllers/user-controllers.js
@@ -48,7 +48,7 @@ const signIn= async(req, res) => {
 const isAuthenticated= async(req, res) => {
     try {
         const token = req.headers['x-access-token'];
-        const response = userService.verifyToken(token);
+        const response = await userService.isAuthenticated(token);
         return res.status(200).json({
             data:response,
             message:'user is authenticated and token is valid',
