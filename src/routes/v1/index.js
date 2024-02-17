469ce5798a73bf6cfd7ed5c5f5bd714b9da08205
Himanshu-Mendhe <himanshu.afcwp23@sinhgad.edu>
2024-02-17 13:19:37 +0530

    added email and password for incoming requst

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const userController = require('../../controllers/user-controllers')
+const userController = require('../../controllers/user-controllers'); 
+const {AuthRequestValidatorMiddlewares} = require('../../middlewares/index');
+
 const router= express.Router();
 
-router.post('/signup', userController.create);
+router.post(
+    '/signup',
+    AuthRequestValidatorMiddlewares.validateUserAuth,
+    userController.create
+    );
 
-router.post('/signin', userController.signIn);
+router.post(
+    '/signin',
+    AuthRequestValidatorMiddlewares.validateUserAuth,
+    userController.signIn
+    );
 
 module.exports = router;
