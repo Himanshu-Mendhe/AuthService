90f6937fe0f047f024d95ce53edcb0374178c315
Himanshu-Mendhe <himanshu.afcwp23@sinhgad.edu>
2024-02-18 13:40:51 +0530

    added Validation error class

--- src/controllers/user-controllers.js
+++ src/controllers/user-controllers.js
@@ -16,11 +16,11 @@ const create= async(req,res) => {
         })
     } catch (error) {
         console.log(error);
-        return res.status(500).json({
+        return res.status(error.statusCode).json({
             data: {},
             success: false,
-            err: error,
-            message: 'unable to create a user'
+            err: error.explanation,
+            message: error.message
         })
     }
 }
